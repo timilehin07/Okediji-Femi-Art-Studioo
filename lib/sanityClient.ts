9d5aa9f999e6954b28f8eb3d1bd9c3432366ca43
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-18 00:52:38 +0100

    Update sanityClient.ts

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@sanity/client";
+import { createClient } from '@sanity/client'
 
-export const sanityClient = createClient({
-  projectId: "dhod8pfr",
-  dataset: "production",
-  apiVersion: "2025-12-17",
-  useCdn: false,
-});
+export const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: '2024-01-01',
+  useCdn: true,
+})
