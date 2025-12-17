f2e2adb2d41dc9a02843340411904dce0d4d02c1
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-17 23:52:51 +0100

    Update sanityClient.ts

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,8 +1,8 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: 'dhod8pfr', // from your Sanity Studio config
-  dataset: 'production',
-  apiVersion: '2025-12-17', // today's date or your chosen API version
-  useCdn: true,
-})
+  projectId: "dhod8pfr",
+  dataset: "production",
+  apiVersion: "2025-12-17",
+  useCdn: false,
+});
