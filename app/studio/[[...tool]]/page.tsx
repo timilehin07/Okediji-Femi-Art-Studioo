2881d0a9b5ea748633f7bc1a0d14e6b9dd93e9bc
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-18 00:06:52 +0100

    Delete app/studio/[[...tool]] directory

--- app/studio/[[...tool]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../lib/sanity.config'
-
-export const dynamic = 'force-static'
-export { metadata, viewport } from 'next-sanity/studio'
-
-export default function StudioPage() {
-  return <NextStudio config={config} />
-}
