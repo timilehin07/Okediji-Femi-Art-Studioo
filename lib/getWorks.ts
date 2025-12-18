16d3f0ad042fdcd2e0adceef95d1cf18b693bdb8
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-18 01:07:49 +0100

    Update getWorks.ts

--- lib/getWorks.ts
+++ lib/getWorks.ts
@@ -1,4 +1,10 @@
-import { client } from "./sanity"
+import { client } from "./sanityClient"
+xport async function getWorks() {
+  console.log("PROJECT ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID)
+  console.log("DATASET:", process.env.NEXT_PUBLIC_SANITY_DATASET)
+
+  return client.fetch(`*[_type == "work"]{..., image}`)
+}
 
 export async function getWorks() {
   return client.fetch(`
