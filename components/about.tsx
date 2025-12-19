b996659d578a40d614fc90e049f0ee81a8be7b87
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-19 01:57:40 +0100

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -136,7 +136,6 @@ export default async function AboutPage() {
                     ))}
                   </ul>
                 </div>
-              )}
             </div>
           </div>
               
