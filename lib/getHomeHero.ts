a132057d4ae9b3dce4c3cbf6dca6544ab16c3624
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-19 14:37:35 +0100

    Update getHomeHero.ts

--- lib/getHomeHero.ts
+++ lib/getHomeHero.ts
@@ -1,9 +1,13 @@
 import { client } from "./sanityClient"
 
 export async function getHomeHero() {
-  return client.fetch(`
+  return client.fetch(
+    `
     *[_type == "home"][0]{
       heroImage
     }
-  `)
+    `,
+    {},
+    { cache: "no-store" }
+  )
 }
