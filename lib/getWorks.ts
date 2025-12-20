9d94889c2b7e34c58cb8d6c105ca369593bf807c
Afolayan Tim <afolayantimi555@gmail.com>
2025-12-20 15:30:27 +0100

    Update getWorks.ts

--- lib/getWorks.ts
+++ lib/getWorks.ts
@@ -1,14 +1,14 @@
 import { client } from "./sanityClient"
 
-interface Work {
+export interface Work {
   _id: string
   title: string
-  year: number
-  material: string
+  year?: string
+  material?: string
   price?: number
   status: "available" | "sold"
   description?: string
-  image?: any
+  images: any[]
 }
 
 export async function getWorks(): Promise<Work[]> {
@@ -22,10 +22,13 @@ export async function getWorks(): Promise<Work[]> {
       price,
       status,
       description,
-      image
+      images
     }
     `,
     {},
-    { cache: "no-store" }
+    {
+      cache: "no-store",
+      next: { revalidate: 0 },
+    }
   )
 }
