47216472ce4cff2cafcafb79644696132e7ee4ab
Afolayan Tim <afolayantimi555@gmail.com>
2026-06-01 18:35:40 +0100

    Update getWorks.ts

--- lib/getWorks.ts
+++ lib/getWorks.ts
@@ -1,20 +1,6 @@
 import { client } from "./sanityClient"
 import { Work } from "./types"
 
-export interface Work {
-_id: string
-  title: string
-  year?: string
-  material?: string
-  price?: {
-    amount: number
-    currency: "USD" | "NGN"
-  } | null
-  status: "available" | "sold"
-  description?: string
-  images: any[]
-}
-
 export async function getWorks(): Promise<Work[]> {
   return client.fetch(
     `
