7f81a931ad066a6290e48fdc7dd1c2a13a5a57b9
Afolayan Tim <afolayantimi555@gmail.com>
2026-07-11 20:08:36 +0100

    Update portfolio.tsx

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -259,7 +259,7 @@ export default function Portfolio({ works, showSeeMore = true, seeMoreHref = "/w
               </button>
             </Link>
           </div>
-        )}g
+        )}
 
       </div>
     </section>
